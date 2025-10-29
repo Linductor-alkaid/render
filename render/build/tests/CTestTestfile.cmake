80a0d4a203c4350ba957ff1095302eb8a0043dcc
Linductor <202200171251@mail.sdu.edu.com>
2025-10-29 23:29:16 +0800

    修复一下git仓库位置错误的问题

--- render/build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/myproject/render/tests
-# Build directory: G:/myproject/render/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
